be210c0c462fd7c28a5630ae572f4cb27108a031
MicheleRai <rainonemichele1999@gmail.com>
2023-03-30 14:43:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Data.SqlClient;
+
+
+var connStr = "Data Source=localhost; Initial Catalog = videogiochi - db; Integrated Security = True";
+
+SqlConnection connessioneSql = new SqlConnection(connStr);
